3e943af8627e7c73c520aaaf0a7f77776bda08d3
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2019-10-13 22:59:49 +0100

    Mostly done, just needs tidied up

--- assignment1/index.js
+++ assignment1/index.js
@@ -5,12 +5,12 @@ app.use(express.static('public'));
 const fetch = require("node-fetch");
 const api = '83ed0a936b1c720ba5289204836966c2';
 
-app.get("/getWeather/:location", (request, response) => {
+app.get("/getForecast/:location", (request, response) => {
     let location = request.params.location;
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + api)
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&APPID=' + api)
     .then((res) => res.json())
     .then((data) => {
         response.send(data);
     })
-    .catch(err => console.log("Something went wrong"))
+    .catch(err => console.log("Something went wrong: ", err))
 });
